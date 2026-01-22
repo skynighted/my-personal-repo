cd27e3d163cda967a4fcf020241649836ef16708
wpr <16016598+process-revolution@user.noreply.gitee.com>
2026-01-22 12:16:28 +0800

    wpr提交了day04代码

---
+++ appCity/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="container">
+
+  </div>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ appCity/src/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>大屏</div>
+</template>
+<script setup>
+
+</script>
+<style>
+
+</style>
